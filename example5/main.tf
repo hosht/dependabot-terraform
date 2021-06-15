fb8dbdfe7a331ee1c58caacdf8b0b31d633a7bf0
Takanobu Hoshino <hosh.tknb@gmail.com>
2021-06-16 00:39:56 +0900

    Add a module example

---
+++ example5/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = "1.0.0"
+
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+resource "null_resource" "null" {}
+
+module "files" {
+  source  = "matti/resource/shell"
+  version = "1.1.0"
+
+  command = "ls -l"
+}
